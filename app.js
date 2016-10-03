f392b0d2a9a624a2f2da41d97e49d5fa839c8ee8
Tamara <merrick1988@gmail.com>
2016-10-03 14:39:07 +0300

    add cache

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ if ('serviceWorker' in window.navigator) {
     window.navigator.serviceWorker.
     register('./sw.js').then((registration) => {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-}).
+    }).
     catch((err) => {
         console.log('ServiceWorker registration failed: ', err);
-});
+    });
 }
+
+var urlsToCache = ['/app.js'];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
